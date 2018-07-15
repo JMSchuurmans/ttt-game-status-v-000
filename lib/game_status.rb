35489c7f832a8daddec73f47e6b0f4e9dec41e66
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-07-15 05:44:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -44,3 +44,59 @@
 # end
 # end
 # >>>>>>> afebf735ef9caeccb5da8f21d38a4666dcb06f15
+
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
+def won?(board)
+  WIN_COMBINATIONS.any? do |index|
+
+    if board[index[0]] == "X" &&
+      board[index[1]] == "X" &&
+      board[index[2]] == "X" ||
+      board[index[0]] == "O" &&
+      board[index[1]] == "O" &&
+      board[index[2]] == "O"
+      return index
+    else
+      false
+    end
+  end
+end
+
+def full?(board)
+ if board.any? {|i| i == " "}
+  false
+ else
+  true
+  end
+end
+
+def draw?(board)
+  !won?(board)
+end
+
+def over?(board)
+  draw?(board) || won?(board) || full?(board)
+end
+
+def winner(board)
+    if won?(board)
+       winning_line = won?(board)
+       return board[winning_line[0]]
+    end
+end
